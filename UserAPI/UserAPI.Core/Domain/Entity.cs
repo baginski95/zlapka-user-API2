417422a555506e7e5cad4d7b0d019d08dd45a670
baginski95 <bagiczny3@gmail.com>
2020-12-10 10:46:06 +0100

    Created project with Solution -> 2x liblary class as projects and UserAPI project

---
+++ UserAPI/UserAPI.Core/Domain/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserAPI.Core.Domain
+{
+    class Entity
+    {
+    }
+}
---
+++ UserAPI/UserAPI.Core/Domain/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserAPI.Core.Domain
+{
+    class User
+    {
+        public int Id { get; protected set; }
+        public int PhoneNumber { get; protected set; } // for a while be a string 
+        public int Score { get; protected set; }
+        public DateTime DateOfBirth { get; protected set; }
+        public string UserName { get; protected set; }
+        public string FirstName { get; protected set; }
+        public string SecondName { get; protected set; }
+        public string Description { get; protected set; }
+        public string Email { get; protected set; }
+        public string PhotoDir { get; protected set; }
+        public string Role { get; protected set; }
+        public IEnumerable<EventHeader> UserEvents { get; set; }
+
+        //public IEnumerable<Preference> Preferences{ get; set;} 
+    }
+}
